1ac0599bc204b26c1a1e7fa6f0cb212103051dd6
Mariklimova <mariku22@yandex.ru>
2023-09-09 09:19:35 +0300

    addition

--- 020. Functions/10/app.js
+++ 020. Functions/10/app.js
@@ -3,4 +3,28 @@
 // результат функции проверки – true, то вызывать новую функцию, возвращающую
 // массив с четными элементами массива
 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+function checkNumber(arr) {
+    const res = arr.every(function (elem) {
+        if (!isNaN(elem)) {
+            return true
+        }
+    })
+    return res
+}
+
+function chooseEvenNumbers(arr) {
+    const check = checkNumber(arr);
+    if (check === true) {
+        let res2 = arr.filter(function (el) {
+            if (el % 2 === 0) {
+                return true
+            }
+        })
+        return res2
+    }
+
+}
+let res3 = chooseEvenNumbers(arr);
+console.log(res3);
--- 020. Functions/11/app.js
+++ 020. Functions/11/app.js
@@ -1,4 +1,28 @@
 // 11. На входе число. Необходимо создать функцию, возвращающую факториал числа
 // 4! = 1 * 2 * 3 * 4
 
+const n = 4;
 
+// 2
+// function factorial(n) {
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result *= i
+//     }
+//     return result
+// }
+// const fact = factorial(n);
+// console.log(fact);
+
+
+
+// 2
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1
+    } else {
+        return n * factorial(n - 1)
+    }
+}
+const fact = factorial(n);
+console.log(fact);
