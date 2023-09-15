bc03f3133e4e4e94fc0d36e9603bda0ccd1be08f
Mariklimova <mariku22@yandex.ru>
2023-09-15 20:21:29 +0300

    addition

--- 021. Context/9/app.js
+++ 021. Context/9/app.js
@@ -1,3 +1,18 @@
 // 7. Необходимо реализовать конкатенацию строк при каждом последующем вызове
 // функции с использованием замыкания
 
+function concatStr() {
+    let str = '';
+    return (function (a) {
+        str += a + ' '
+        console.log(str);
+    })
+
+}
+let res = concatStr()
+res('Я');
+res('учусь');
+res('в');
+res('HSchool');
+
+
