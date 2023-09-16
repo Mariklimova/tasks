ef8d430cca59ce9739a833dd61e8266afc10b507
Mariklimova <mariku22@yandex.ru>
2023-09-16 21:50:54 +0300

    addition

--- 021. Context/10/app.js
+++ 021. Context/10/app.js
@@ -4,3 +4,13 @@
 
 const arr = [1, 1, 2, 2, 2, 3, 4, 5, 5, 5];
 
+let result = (function (arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!newArr.includes(arr[i])) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}(arr))
+console.log(result);
