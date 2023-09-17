e3b1293a6882658d396892b69d002a6a3e221013
Mariklimova <mariku22@yandex.ru>
2023-09-17 21:29:21 +0300

    addition

--- 021. Context/7/app.js
+++ 021. Context/7/app.js
@@ -2,7 +2,18 @@
 // является ли этот пароль надежным. Надежный пароль должен содержать хотя бы
 // одну заглавную букву, цифры, а его длина должна быть не менее 8 символов
 
-const str = 'Agata111213'
-function name(params) {
-    
+let str = 'Agata111213'
+
+
+function checkPassword(str) {
+    if (str.length < 8) return 'пороль меньше восьми символов'
+    for (let i = 0; i < str.length; i++) {
+        if (isNaN(str[i]) && str[i] !== str[i].toUpperCase()) {
+            return 'пороль не надежный'
+        } else {
+            return 'пороль надежный'
+        }
+    }
 }
+const res = checkPassword(str);
+console.log(res);
