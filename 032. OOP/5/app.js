1e90e85d70b04431535162b52fb4fdfd44356c3c
Mariklimova <mariku22@yandex.ru>
2023-10-17 02:22:35 +0300

    added tasks 5,8

--- 032. OOP/5/app.js
+++ 032. OOP/5/app.js
@@ -2,15 +2,99 @@
 //     замены букв исходного слова или фразы). Создать функцию для вывода ряда true,
 //     если слова являются анаграммами. Добавить проверки на ввод
 
+
+// 1
+// class Anagram {
+//     word_1 = 'сосна';
+//     word_2 = 'насос';
+//     isValid() {
+//         if (!isNaN(this.word_1 && this.word_2)) throw new Error('not a string')
+//     }
+//     checkAnagram = () => {
+//         try {
+//             this.isValid();
+//             if (this.word_1.split('').sort().join('') === this.word_2.split('').sort().join('')) return true
+//             else return false
+//         } catch (error) {
+//             return error.message
+//         }
+
+//     }
+// }
+// const anagram = new Anagram();
+// console.log(anagram.checkAnagram());
+
+
+// 2
+// class Anagram {
+//     word_1;
+//     word_2;
+
+//     isValid() {
+//         if (!isNaN(this.word_1 && this.word_2)) throw new Error('not a string')
+//     }
+
+//     checkAnagram = () => {
+//         try {
+//             this.isValid();
+//             if (this.word_1.split('').sort().join('') === this.word_2.split('').sort().join('')) return true
+//             else return false
+//         } catch (error) {
+//             return error.message
+//         }
+
+//     }
+// }
+// const anagram = new Anagram();
+// anagram.word_1 = 'сосна';
+// anagram.word_2 = 'насос';
+// console.log(anagram.checkAnagram());
+
+
+// 3
 class Anagram {
-    word_1 = 'сосна'.split('');
-    word_2 = 'насос'.split('');
-    checkAnagram = () => {
-        if (this.word_1.includes(this.word_2)) {
-            return true
-        } else {
-            return false
+    isValid(word_1, word_2) {
+        if (!isNaN(word_1 && word_2)) throw new Error('not a string')
+    }
+
+    checkAnagram = (word_1, word_2) => {
+        try {
+            this.isValid(word_1, word_2);
+            if (word_1.split('').sort().join('') === word_2.split('').sort().join('')) return true
+            else return false
+        } catch (error) {
+            return error.message
         }
 
     }
 }
+const anagram = new Anagram();
+console.log(anagram.checkAnagram('сосна', 'насос'));
+
+
+// 4
+// class Anagram {
+//     word_1;
+//     word_2;
+//     constructor(str1, str2) {
+//         this.word_1 = str1;
+//         this.word_2 = str2;
+//     }
+//     isValid() {
+//         if (!isNaN(this.str1 && this.str2)) throw new Error('not a string')
+//     }
+//     checkAnagram = () => {
+//         try {
+//             this.isValid();
+//             if (this.str1.split('').sort().join('') === this.str2.split('').sort().join('')) return true
+//             else return false
+//         } catch (error) {
+//             return error.message
+//         }
+
+//     }
+// }
+// const anagram = new Anagram();
+// anagram.str1 = 'сосна';
+// anagram.str2 = 'насос';
+// console.log(anagram.checkAnagram());
--- 032. OOP/8/app.js
+++ 032. OOP/8/app.js
@@ -2,5 +2,73 @@
 // строку и проверяет, является ли она корректным емейлом или нет. Если является
 // - возвращает true, если не является - то false.
 
+// 1
+// class Validator {
+//     mail = 'mariku22@yandex.ru';
+//     isEmail() {
+//         try {
+//             if (!/^[\w\-\/\]+\@[a-z]+\.[a-z]{1,5}$/gm.test(this.mail)) throw new Error('email is not entered correctly')
+//             return true;
+//         } catch (error) {
+//             return error.message
+//         }
+//     }
+
+// }
+// const validator = new Validator();
+// console.log(validator.isEmail());
 
 
+// 2
+// class Validator {
+//     mail;
+//     isEmail() {
+//         try {
+//             if (!/^[\w\-\/\]+\@[a-z]+\.[a-z]{1,5}$/gm.test(this.mail)) throw new Error('email is not entered correctly')
+//             return true;
+//         } catch (error) {
+//             return error.message
+//         }
+//     }
+
+// }
+// const validator = new Validator();
+// validator.mail = 'mariku22@yandex.ru';
+// console.log(validator.isEmail());
+
+
+// 3
+// class Validator {
+//     isEmail(mail) {
+//         try {
+//             if (!/^[\w\-\/\]+\@[a-z]+\.[a-z]{1,5}$/gm.test(mail)) throw new Error('email is not entered correctly')
+//             return true;
+//         } catch (error) {
+//             return error.message
+//         }
+//     }
+
+// }
+// const validator = new Validator();
+// console.log(validator.isEmail('mariku22@yandex.ru'));
+
+
+// 4
+class Validator {
+    email;
+    constructor(mail) {
+        this.email = mail;
+    }
+    isEmail() {
+        try {
+            if (!/^[\w\-\/\]+\@[a-z]+\.[a-z]{1,5}$/gm.test(this.mail)) throw new Error('email is not entered correctly')
+            return true;
+        } catch (error) {
+            return error.message
+        }
+    }
+
+}
+const validator = new Validator();
+validator.mail = 'mariku22@yandex.ru';
+console.log(validator.isEmail());
