bafc5c1c7b2677644b9dbf7995f9c54b6ee287a2
Mariklimova <mariku22@yandex.ru>
2023-10-19 20:11:52 +0300

    addition to tasks 031

--- 031. Proto/9/app.js
+++ 031. Proto/9/app.js
@@ -1,4 +1,23 @@
 // 9. На входе объект. Ключи и значения - автоинкремент (генерируется автоматически
 //     от 1 до n). Необходимо вывести количество пар ключ значение объекта)
 
-const n = 
+
+const n = Math.round(Math.random() * 10);
+
+function doObject(n) {
+    const obj = {};
+    for (let i = 0; i < n; i++) {
+        obj[i] = Math.round(Math.random() * 10);
+    }
+    return obj;
+}
+function numberPairs(obj) {
+    doObject();
+    let check = 0;
+    for ( key in obj) {
+        if(obj.key) check++
+    }
+    return check
+}
+const res = numberPairs(obj)
+console.log(res);
