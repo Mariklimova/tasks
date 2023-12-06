b92034cf6110b21d21daa8e5646e0bf37e5850e2
Mariklimova <mariku22@yandex.ru>
2023-12-06 23:25:20 +0300

    additoin tasks 050

--- 050.node.js/index.js
+++ 050.node.js/index.js
@@ -11,15 +11,38 @@ app.get('/', (req, res) => {
 })
 app.get('/about', (req, res) => {
     const mail = 'mariku22@yandex.ru';
-    if(/^[\w]+\@[a-z]+\.[a-z]{1,6}/gm.test(mail)) res.send(true)
+    if (/^[\w]+\@[a-z]+\.[a-z]{1,6}/gm.test(mail)) res.send(true)
 })
 app.get('/contact', (req, res) => {
     res.send('contact')
 })
 
+// 7
+app.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const arr = [1, 2, 3, 4, 5, 6];
+    const resArr = [];
+    let itemArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        itemArr.push(arr[i]);
+        if (itemArr.length == id) {
+            resArr.push(itemArr);
+            itemArr = [];
+        }
+    }
+    res.send(resArr);
+})
 
-app.get('/:id', (req,res)=>{
-
+app.get('/task/:id', (req, res) => {
+    const arr = [
+        { "id": 1, "name": "Yesenia", "age": 22 },
+        { "id": 2, "name": "Hanna", "age": 22 },
+        { "id": 3, "name": "Stanislau", "age": 25 },
+        { "id": 4, "name": "German", "age": 18 },
+        { "id": 5, "name": "Maria", "age": 27 }
+    ]
+    const result = arr.filter((el)=>el.id==id);
+    res.send(result)
 })
 
 app.listen(3000, () => {
@@ -49,6 +72,7 @@ app.listen(3000, () => {
 // 6. Создать обработчик маршрута get с url: "/:id “. Необходимо получить значение id и
 // отправить клиенту. (Для того чтобы получить значение из url необходимо
 // воспользоваться деструктуризацией из request.params)
+
 // 7. Создать обработчик маршрута get с url: "/:id“. Необходимо получить значение id. На
 // сервере хранится массив из чисел. Напишите функцию, которая разделяет массив
 // на части заданного размера. Необходимо вернуть клиенту массив
