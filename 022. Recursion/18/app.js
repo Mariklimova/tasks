61946e15a2ccfbeab0ed765507aa6a2c9796b0a7
Mariklimova <mariku22@yandex.ru>
2024-09-17 22:43:55 +0300

    added tasks

---
+++ 022. Recursion/18/app.js
@@ -0,0 +1,23 @@
+// 10. На входе строка s. Проверьте, является ли строка палиндромом (читается одинаково в обе 
+//     стороны), используя рекурсию.
+//     Входные: "madam" → Результат: True
+//     Входные: "hello" → Результат: False
+//     Входные: "racecar" → Результат: True
+
+
+const polindrom = (str) => {
+    if (str.length <= 0) return true;
+    if (str[0] === str[str.length - 1]) return true
+    return (str.slice(1, -1))
+}
+
+console.log(polindrom("madam"));
+console.log(polindrom("hello"));
+console.log(polindrom("racecar"));
+
+
+
+
+
+
+
